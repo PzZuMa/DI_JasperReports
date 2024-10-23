448fd1db06c85fb17eb0870ebdcbd81722fbbd60
PzZuMa <pablo.s349227@cesurformacion.com>
2024-10-23 13:24:26 +0200

    Inicio V.2

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,8 @@ module org.example.retoconjuntohibernate {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.hibernate.orm.core;
-
+    requires static lombok;
+    requires java.desktop;
 
     opens org.example.retoconjuntohibernate to javafx.fxml;
     exports org.example.retoconjuntohibernate;
